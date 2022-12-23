d8457a19ac95aefe796fa716755c359b6c08162e
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-23 11:11:13 +0000

    KeyOf operator

--- exercise_files/Ch03/03_02b/src/demo.ts
+++ exercise_files/Ch03/03_02b/src/demo.ts
@@ -2,7 +2,7 @@ type ContactName = string;
 type ContactStatus = "active" | "inactive" | "new"
 type ContactBirthDate = Date | number | string
 
-interface Contact  {
+interface Contact {
     id: number;
     name: ContactName;
     birthDate?: ContactBirthDate;
@@ -14,3 +14,18 @@ let primaryContact: Contact = {
     name: "Jamie Johnson",
     status: "active"
 }
+
+type ContactFields = keyof Contact;//keyof operator
+
+const contactFields: ContactFields = "name";//It will directly suggest the values of the type=> 'id' | 'name' | 'birthDate' | 'status'
+
+function advanceUsage(sourceObj, propertyName: keyof Contact) {
+    return sourceObj[propertyName];
+}
+advanceUsage(primaryContact, "name");//Only the properties of the Contact type will be suggested
+
+function moreAdvanceUsage<T, K extends keyof T>(sourceObj: T, propertyName: K) {
+    return sourceObj[propertyName];
+}
+moreAdvanceUsage(primaryContact, "name");
+moreAdvanceUsage({min: 10, max: 20},"max");// will suggest "min" | "max
