975fa7fec0058b9086222326002019a71af0f7e1
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-23 09:40:08 +0000

    Type aliases

--- exercise_files/Ch02/02_03b/src/demo.ts
+++ exercise_files/Ch02/02_03b/src/demo.ts
@@ -1,11 +1,19 @@
-interface Contact {
-    id: number;
-    name: string;
-    birthDate?: Date;
-}
+// interface Contact {
+//     id: number;
+//     name: string;
+//     birthDate?: Date;
+// }
 
 let primaryContact: Contact = {
     birthDate: new Date("01-01-1980"),
     id: 12345,
     name: "Jamie Johnson",
+}
+
+type ContactName = string;
+
+interface Contact {
+    id: number;
+    name: ContactName;//string;
+    birthDate?: Date;
 }
