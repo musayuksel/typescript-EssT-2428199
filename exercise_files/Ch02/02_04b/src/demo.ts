7aa98b6a90d47d1fcd73def4de151ea7f6e13bf7
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-23 09:46:18 +0000

    Enums

--- exercise_files/Ch02/02_04b/src/demo.ts
+++ exercise_files/Ch02/02_04b/src/demo.ts
@@ -2,12 +2,20 @@ interface Contact {
     id: number;
     name: ContactName;
     birthDate?: Date;
+    status?: contactStatus;
+}
+
+enum contactStatus {
+    Active = "active",
+    Inactive = "inactive",
+    New = "new",
 }
 
 let primaryContact: Contact = {
     birthDate: new Date("01-01-1980"),
     id: 12345,
     name: "Jamie Johnson",
+    status: contactStatus.Active
 }
 
 type ContactName = string
