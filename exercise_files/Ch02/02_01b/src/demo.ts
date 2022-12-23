4cf655156602cfb72bedec609516e1a1596421f5
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-23 09:23:45 +0000

    add example types

---
+++ exercise_files/Ch02/02_01b/src/demo.ts
@@ -0,0 +1,7 @@
+const stringType :string = "Any string value";
+const numberType :number = 100;
+const booleanType :boolean = true;
+const dateType :Date = new Date();
+//const arrayTypeWrong :number[] ;//error
+const arrayType :number[] = [1,2,3];
+const anyType :any = "Any value";//don't suggest to use any type
