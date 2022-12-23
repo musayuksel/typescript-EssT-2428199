b4067c6782f83603a766e50d8337c8180a50c40c
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-23 10:19:26 +0000

    Generics

--- exercise_files/Ch02/02_06b/src/demo.ts
+++ exercise_files/Ch02/02_06b/src/demo.ts
@@ -3,9 +3,16 @@ interface Contact {
     name: string;
 }
 
-function clone(source: Contact): Contact {
+function clone<T>(source: T): T {
     return Object.apply({}, source);
 }
 
 const a: Contact = { id: 123, name: "Homer Simpson" };
 const b = clone(a)
+
+const objectWithDifferentType = clone({ startDate: new Date() });//all generics <t> will be {startDate: Date}
+
+function cloneAdvance<T1, T2 extends T1>(source: T1): T2 {
+    // return type T2 must be a subtype of T1, plus I can add more properties to T2
+    return Object.apply({}, source);
+}
