3eab714a92529c8daec91fe2de8b338ba12f5a02
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-23 09:35:23 +0000

    Add extends

--- exercise_files/Ch02/02_02b/src/demo.ts
+++ exercise_files/Ch02/02_02b/src/demo.ts
@@ -5,6 +5,30 @@ interface Contact {
   email?: string; //optional
 }
 
+interface Address {
+  street: string;
+  city: string;
+  state: string;
+  postCode: string;
+}
+
+interface contactWithAddress extends Address {
+  id: number;
+  name: string;
+  dob: Date;
+  email?: string; //optional
+}
+
+const contactWithAddress: contactWithAddress = {
+  id: 1,
+  name: 'John',
+  dob: new Date(),
+  street: '123 Main St',
+  city: 'Anytown',
+  state: 'CA',
+  postCode: '12345',
+};
+
 const contact: Contact = {
   id: 1,
   name: 'John',
