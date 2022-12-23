b3fb1bf2be270a6f1d1f7bc4e9f507fa67524f37
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-23 09:59:48 +0000

    Function typing

--- exercise_files/Ch02/02_05b/src/demo.ts
+++ exercise_files/Ch02/02_05b/src/demo.ts
@@ -1,8 +1,14 @@
 interface Contact {
     id: number;
     name: string;
+    cloneMethod: () => Contact;
+    cloneMethod2(contact): Contact;
 }
 
-function clone(source) {
+function clone(source: Contact): Contact {
+    return Object.apply({}, source);
+}
+
+function cloneWithFunctionArguments(source: Contact, cb: () => Contact): Contact {
     return Object.apply({}, source);
 }
