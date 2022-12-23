9e4200aca5277fd495de3bf7ce6e3b2149b9433e
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-23 12:04:33 +0000

    Record type: Dynamic but limited types

--- exercise_files/Ch03/03_05b/src/demo.ts
+++ exercise_files/Ch03/03_05b/src/demo.ts
@@ -1,6 +1,7 @@
-let x = { name: "Wruce Bayne" };
+let x : Record<string,string|number>={ name: "Wruce Bayne" };
 x.id = 1234;
-
+x[5]="five";
+//key should be string and value should be string or number
 
 
 ////////////////////
@@ -25,9 +26,11 @@ interface Query {
     matches(val): boolean;
 }
 
-function searchContacts(contacts: Contact[], query) {
+// function searchContacts(contacts: Contact[], query) {//query is any type
+// function searchContacts(contacts: Contact[], query:Record<string,Query>) {//query can be any object with string keys and Query values
+function searchContacts(contacts: Contact[], query:Record<keyof Contact,Query>) {//query must be an object with Contact keys and Query values
     return contacts.filter(contact => {
-        for (const property of Object.keys(contact)) {
+        for (const property of Object.keys(contact) as (keyof Contact)[]) {//I did't understand this line
             // get the query object for this property
             const propertyQuery = query[property];
             // check to see if it matches
@@ -44,7 +47,7 @@ const filteredContacts = searchContacts(
     [/* contacts */],
     {
         id: { matches: (id) => id === 123 },
-        name: { matches: (name) => name === "Carol Weaver" },
-        phoneNumber: { matches: (name) => name === "Carol Weaver" },
+        name: { matches: (name) => name === "Carol Weaver"},
+        // phoneNumber: { matches: (name) => name === "Carol Weaver" },//We solved this problem by using keyof Contact
     }
 );
