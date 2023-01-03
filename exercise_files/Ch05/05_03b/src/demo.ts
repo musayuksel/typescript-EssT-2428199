6897178b8d7b4fb81747d7a83b6112edf54820e7
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-03 11:08:31 +0000

    Decorator factory function

--- exercise_files/Ch05/05_03b/src/demo.ts
+++ exercise_files/Ch05/05_03b/src/demo.ts
@@ -13,27 +13,28 @@ const currentUser = {
     }
 }
 
-function authorize(target: any, property: string, descriptor: PropertyDescriptor) {
-    const wrapped = descriptor.value
+//To pass a parameter to the decorator, we need to use a factory function
+function authorize(role: string) {
+    return function authorizeDecorator(target: any, property: string, descriptor: PropertyDescriptor) {
+        const wrapped = descriptor.value
 
-    descriptor.value = function () {
-        if (!currentUser.isAuthenticated()) {
-            throw Error("User is not authenticated");
+        descriptor.value = function () {
+            if (!currentUser.isAuthenticated()) {
+                throw Error("User is not authenticated");
+            }
+            if (!currentUser.isInRole(role)) {//role is the parameter passed to the factory function
+                throw Error("User not authorized to execute this action");
+            }
+
+            return wrapped.apply(this, arguments);
         }
-
-        return wrapped.apply(this, arguments);
     }
 }
-
 class ContactRepository {
     private contacts: Contact[] = [];
 
     @authorize("ContactViewer")
     getContactById(id: number): Contact | null {
-        if (!currentUser.isInRole("ContactViewer")) {
-            throw Error("User not authorized to execute this action");
-        }
-
         const contact = this.contacts.find(x => x.id === id);
         return contact;
     }
